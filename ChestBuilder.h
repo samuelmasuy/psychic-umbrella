a7a8840e61afa6855831c00e9c205a1e2d2ae259
Samuel Masuy <smasuy@jive.com>
2016-11-10 18:57:31 -0500

    Added doc for chest, chest builder, chest Director

--- ChestBuilder.h
+++ ChestBuilder.h
@@ -1,17 +1,45 @@
+/**
+ * @file ChestBuilder.h
+ * @brief Definition of the Builder pattern for the chest.
+ */
 #pragma once
 #include "Chest.h"
 #include "ItemDirector.h"
 
-class ChestBuilder
-{
-public:
-	ChestBuilder();
-	void createNewChest() { chest = new Chest(); buildItem(); };
-	Chest* getChest() { return chest; };
-	void buildItem();
-	~ChestBuilder();
+/**
+ * @brief ChestBuilder class, it creates chests
+ */
+class ChestBuilder {
+ public:
+  /**
+   * @brief Constructor for a chest.
+   */
+  ChestBuilder();
+  /**
+   * @brief Build a new chest
+   */
+  void createNewChest() {
+    chest = new Chest();
+    buildItem();
+  }
+  /**
+   * @brief Get chest that was built
+   *
+   * @return a chest
+   */
+  Chest* getChest() {
+    return chest;
+  }
+  /**
+   * @brief Build the Item.
+   */
+  void buildItem();
+  /**
+   * @brief Destructor for ChestBuilder.
+   */
+  ~ChestBuilder();
 
-private:
-	Chest* chest;
+ private:
+  Chest* chest;
 };
 
