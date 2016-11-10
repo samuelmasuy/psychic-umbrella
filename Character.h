526881f2a5de013e452e014cffa083f6409f1a72
Samuel Masuy <smasuy@jive.com>
2016-11-09 21:03:40 -0500

    merge conflicts fixed

--- Character.h
+++ Character.h
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-//! @file 
-//! @brief Header file for the Character class  
-//!
-
-#pragma once
-#include<string>
-#include "Subject.h"
-#include "ItemContainer.h"
-const int MAX_ITEMS_EQUIPPED = 7;
-using namespace std;
-
-//! Class that implements a character 
-class Character : public Subject
-{
-public:
-	Character();
-	Character(int, int, int, int, int, int);
-	bool validateNewCharacter();
-	void hit(int);
-	int generateStats();
-	int getHitPoints();
-	void hpChange();
-	int abilityModifier(int);
-	void setLevel(int);
-	int armorModifier();
-	int attackBonus();
-	int damageBonus();
-	void saveCharacter();
-	void loadCharacter();
-	void playerInfo();
-	bool equipItem(Item*);
-	bool unequipItem(string);
-	void printBackPackItems();
-	void addToBackpack(Item);
-	void printEquippedItems();
-private: 
-	string characterType;
-	int abilityScores[6];
-	int currentHitPoints;
-	Item equipment[MAX_ITEMS_EQUIPPED];
-	ItemContainer backpack;
-	enum equipmentSlots {Helmet, Armor, Weapon, Shield, Ring, Belt, Boots};
-	int level;
-	const Item emptyItem; //itemholder for equipementSlots
-};
-
-=======
 //! @file 
 //! @brief Header file for the Character class  
 //!
@@ -127,5 +79,3 @@ public:
     void playerInfo();//overrides parent function
     int attackBonus();
 };
-
->>>>>>> origin/master
