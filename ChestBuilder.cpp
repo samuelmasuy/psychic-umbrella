aa18756d92031f681c3d524c60a46c6a76b8cf67
Samuel Masuy <smasuy@jive.com>
2016-11-10 19:40:56 -0500

    fixed merge conflicts

--- ChestBuilder.cpp
+++ ChestBuilder.cpp
@@ -7,18 +7,11 @@
 ChestBuilder::ChestBuilder() {
 }
 
-<<<<<<< HEAD
 void ChestBuilder::buildItem(){
 	ItemBuilder* randomItemBuilder = new RandomBuilder();
 	randomItemBuilder->createNewItem();
 	randomItemBuilder->buildType();
 	chest->setChestContent(randomItemBuilder->getItem());
-=======
-void ChestBuilder::buildItem() {
-  ItemBuilder* randomItemBuilder = new RandomBuilder();
-  randomItemBuilder->createNewItem();
-  chest->setChestContent(randomItemBuilder->getItem());
->>>>>>> origin/master
 }
 
 ChestBuilder::~ChestBuilder() {
