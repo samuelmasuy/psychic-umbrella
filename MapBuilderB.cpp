026515c225cffa2e55030e9f25cf69acf39e23c4
asmalaaribi <asmalaaribi@gmail.com>
2016-11-10 21:58:43 -0500

    updated validation

--- MapBuilderB.cpp
+++ MapBuilderB.cpp
@@ -27,7 +27,7 @@ MapBuilderB::MapBuilderB(Map &m)
 ///
 MapBuilderB::~MapBuilderB()
 {
-	FreeMem();
+	//FreeMem();
 }
 ///
 /// Set to return the size of the map
@@ -117,7 +117,7 @@ int MapBuilderB::LoadMap(const char *filename, int levelNumber)
 				if (line.size() < (unsigned int)m_cols)
 				{
 					cout << "Invalid Map: Invalid line #" << lineNumber << "." << m_cols << " characters expected instead of " << line.size() << endl;
-					FreeMem();
+					//FreeMem();
 					return 3;
 				}
 				for (int j = 0; j < m_cols; j++)
@@ -125,7 +125,7 @@ int MapBuilderB::LoadMap(const char *filename, int levelNumber)
 					if (0)
 					{
 						cout << "Invalid Map: Character (" << line[j] << ") at line #" << lineNumber << endl;
-						FreeMem();
+						//FreeMem();
 						return 4;
 					}
 					else
@@ -135,7 +135,7 @@ int MapBuilderB::LoadMap(const char *filename, int levelNumber)
 						if (x == CHAR_PLAYER || x == CHAR_ENTRY || x == CHAR_EXIT)
 						{
 							cout << "Invalid map: player, entrance and exit should not be specified again in the map" << endl;
-							FreeMem();
+							//FreeMem();
 							return 4;
 						}
 					}
@@ -166,6 +166,87 @@ int MapBuilderB::LoadMap(const char *filename, int levelNumber)
 	}
 	return 0;
 }
+
+int MapBuilderB::ValidateMap()
+{
+	if (m_rows <= 0 || m_cols <= 0)
+	{
+		cout << "Invalid map. It is empty. Please, load a map from disk, or create a new one!" << endl;
+		return 1;
+	}
+	if (m_i0 < 0 || m_i0 >= m_rows || m_j0 < 0 || m_j0 >= m_cols)
+	{
+		cout << "Invalid map. The entry position has not a valid position" << endl;
+		return 1;
+	}
+	if (m_i1 < 0 || m_i1 >= m_rows || m_j1 < 0 || m_j1 >= m_cols)
+	{
+		cout << "Invalid map. The exit position has not a valid position" << endl;
+		return 1;
+	}
+	if (m_iPlayer < 0 || m_iPlayer >= m_rows || m_jPlayer < 0 || m_jPlayer >= m_cols)
+	{
+		cout << "Invalid map. The player position has not a valid position" << endl;
+		return 1;
+	}
+
+	for (int i = 0; i < m_rows; i++)
+	{
+		for (int j = 0; j < m_cols; j++)
+		{
+			switch (m_scene[i][j])
+			{
+			case CHAR_ENTRY:
+			case CHAR_EXIT:
+			case CHAR_PLAYER:
+				cout << "Entry, exit or player should not be specified in the map; only in the header" << endl;
+				return 2;
+			}
+		}
+	}
+	if (!PathExist(m_i0, m_j0, m_i1, m_j1))
+	{
+		cout << "Scene not valid. There is not a path between entrance and exit" << endl;
+		return 4;
+	}
+	return 0;
+}
+///
+/// determine if a path exits between (row0, col0) and (row1, col1)
+///
+bool MapBuilderB::PathExist(int row0, int col0, int row1, int col1)
+{
+	CELL_TYPE **visited = AllocMem(m_rows, m_cols);
+
+	bool ret = Backtracking(visited, row0, col0, row1, col1);
+	FreeMem(visited, m_rows);
+	return ret;
+}
+///
+/// backtracking to find a path between [row0,col0] and [row1,col1]
+///
+bool MapBuilderB::Backtracking(CELL_TYPE **v, int row0, int col0, int row1, int col1)
+{
+	if (row0<0 || row0 >= m_rows || col0<0 || col0 >= m_cols)
+		return false;
+	if (v[row0][col0])	
+		return false;
+	///
+	/// we cannot pass thru a wall
+	///
+	if (m_scene[row0][col0] == CHAR_WALL)
+		return false;
+	if (row0 == row1 && col0 == col1)
+		return true;
+	v[row0][col0] = true;
+	bool ret =
+		Backtracking(v, row0 + 1, col0, row1, col1) ||
+		Backtracking(v, row0 - 1, col0, row1, col1) ||
+		Backtracking(v, row0, col0 + 1, row1, col1) ||
+		Backtracking(v, row0, col0 - 1, row1, col1);
+	v[row0][col0] = false;
+	return ret;
+}
 ///
 /// Saving the map here
 ///
@@ -190,4 +271,12 @@ int MapBuilderB::SaveLevel(const char *filename)
 	fclose(f);
 	return 0;
 }
-
+void MapBuilderB::FreeMem(CELL_TYPE **x, int rows)
+{
+	if (x && rows>0)
+	{
+		for (int i = 0; i < rows; i++)
+			delete[] x[i];
+		delete[] x;
+	}
+}
