e20ff098505646dc454ea7773de86c4af1f96982
asmalaaribi <asmalaaribi@gmail.com>
2016-11-10 22:01:11 -0500

    updated validation and comments

--- MapEditor.h
+++ MapEditor.h
@@ -1,3 +1,28 @@
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+/// Our Game & Design Info: This is our MapEditor class///
+/// where we can edit the maps we would like to use in ///
+/// our campaigns, we also validate them.              ///
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+/// LIBRARIES USED:                                    ///
+/// iosteam: This is used for our basic input/output   ///
+/// services for our project such as cin and cout.     ///                
+///fstream: This is another input/output stream used   ///
+/// for parameters such as char_type                   ///
+///	vector: This is used for the arrays and all its///
+/// related member functions such as size, max, empty. ///
+/// stdlib: This is used for memory management.        ///
+/// string: Used for string types                      ///
+///                                                    ///
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+
+
 #pragma once
 
 #include "Map.h"
@@ -6,17 +31,23 @@
 class MapEditor
 {
 public:
-	// default constructor
+	///
+	/// default constructor
+	///
 	MapEditor();
-
-	// destructor
+	///
+	/// destructor
+	///
 	~MapEditor();
-
-	// edit the current map 
+	///
+	/// edit the current map 
+	///
 	void EditMap(Map *pMap, const string &filename);
 
 private:
-	// flush the keyboard buffer
+	///
+	/// flush the keyboard buffer
+	///
 	void Flush();
 
 };
