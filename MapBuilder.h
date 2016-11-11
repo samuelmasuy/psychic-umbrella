d00a94c4e9b0e5bdca4515cc7799e8a06aedff85
asmalaaribi <asmalaaribi@gmail.com>
2016-11-10 23:11:06 -0500

    updated comments

--- MapBuilder.h
+++ MapBuilder.h
@@ -6,47 +6,61 @@
 class MapBuilder
 {
 public:
-	// default constructor
+	///
+	/// default constructor
+	///
 	MapBuilder();
-
-	// destructor
+	///
+	/// destructor
+	///
 	virtual ~MapBuilder();
-
-	// return the size of the map
+	///
+	/// return the size of the map
+	///
 	virtual void GetSize(int &rows, int &cols) = 0;
-
-	// return the player position
+	///
+	/// return the player position
+	///
 	virtual void GetPlayerPos(int &i, int &j) = 0;
-
-	// return the entrance position
+	///
+	/// return the entrance position
+	///
 	virtual void GetEntrancePos(int &i, int &j) = 0;
-
-	// return the exit position
+	///
+	/// return the exit position
+	///
 	virtual void GetExitPos(int &i, int &j) = 0;
-
-	// return the array
+	///
+	/// return the array
+	///
 	virtual CELL_TYPE **Get2DArray() = 0;
 
 protected:
-
-	// size of the map
+	///
+	/// size of the map
+	///
 	int m_rows, m_cols;
-
-	// entrance position
+	///
+	/// entrance position
+	///
 	int m_i0, m_j0;
-
+	///
 	// exit position
+	///
 	int m_i1, m_j1;
-
-	// player position
+	///
+	/// player position
+	///
 	int m_iPlayer, m_jPlayer;
-
+	///
 	CELL_TYPE **m_scene;
-
-	// release memory of m_scene
+	///
+	/// release memory of m_scene
+	///
 	void FreeMem();
-
-	// alloc memory for a 2D array
+	///
+	/// alloc memory for a 2D array
+	///
 	CELL_TYPE **AllocMem(int rows, int cols);
 
 };
